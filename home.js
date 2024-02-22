d11c740973809eee509f6ed6b338491bab60de73
Oladele-israel <omoleoladele43@gmail.com>
2024-02-22 16:16:35 +0100

    updating the media query

--- home.js
+++ home.js
@@ -1,31 +1,31 @@
 const paymentOption = [
-    {
-        name: 'quick delivery',
-        tagline: 'fast & easy',
-        img: './images/quick-delivery.png'
-    },
+  {
+    name: "quick delivery",
+    tagline: "fast & easy",
+    img: "./images/quick-delivery.png",
+  },
 
-    {
-        name: 'Bill Payment',
-        tagline: 'quick transfer',
-        img: './images/bill-payment.png'
-    },
-    {
-        name: 'transfer funds',
-        tagline: 'send and recieve',
-        img: './images/transfer-funds.png'
-    },    
-    {
-        name: 'pay later',
-        tagline: 'buy and pay later',
-        img: './images/pay-later.png'
-    }
-]
+  {
+    name: "Bill Payment",
+    tagline: "quick transfer",
+    img: "./images/bill-payment.png",
+  },
+  {
+    name: "transfer funds",
+    tagline: "send and recieve",
+    img: "./images/transfer-funds.png",
+  },
+  {
+    name: "pay later",
+    tagline: "buy and pay later",
+    img: "./images/pay-later.png",
+  },
+];
 
 const cardDisplay = document.querySelector(".card");
 
-paymentOption.map((option)=>{
-    cardDisplay.innerHTML += `
+paymentOption.map((option) => {
+  cardDisplay.innerHTML += `
     <div class="card1">
     <img src=${option.img} alt="">
     <div>
@@ -33,39 +33,39 @@ paymentOption.map((option)=>{
       <span>${option.tagline}</span>
     </div>
   </div>
-    `
-})
+    `;
+});
 
 const topSellingProduct = [
-    {
-        name: 'sem0- vitta ',
-        price: '$300',
-        img: './images/product-images/semo-vitta.png.png',
-        tag: 'just bought'
-    },
+  {
+    name: "sem0- vitta ",
+    price: "$300",
+    img: "./images/product-images/semo-vitta.png.png",
+    tag: "just bought",
+  },
 
-    {
-        name: 'goli oil ',
-        price: '$200',
-        img: './images/product-images/goli-oil.png.png',
-        tag: 'just bought'
-    },
-    {
-        name: 'mamador oil ',
-        price: '$300',
-        img: './images/product-images/mamador-oil.png.png',
-        tag: 'just bought'
-    },
-    {
-        name: 'yam-tuber ',
-        price: '$30',
-        img: './images/product-images/yam-tuber.png.png',
-    }
-]
+  {
+    name: "goli oil ",
+    price: "$200",
+    img: "./images/product-images/goli-oil.png.png",
+    tag: "just bought",
+  },
+  {
+    name: "mamador oil ",
+    price: "$300",
+    img: "./images/product-images/mamador-oil.png.png",
+    tag: "just bought",
+  },
+  {
+    name: "yam-tuber ",
+    price: "$30",
+    img: "./images/product-images/yam-tuber.png.png",
+  },
+];
 
-const products = document.querySelector('.products-list');
-topSellingProduct.map((product)=>{
-    products.innerHTML +=`
+const products = document.querySelector(".products-list");
+topSellingProduct.map((product) => {
+  products.innerHTML += `
     <div class="product1">
     <div class="product-image">
       <img src=${product.img} alt="">
@@ -78,12 +78,47 @@ topSellingProduct.map((product)=>{
    </div>
     </div>
   </div>
-    `
-})
+    `;
+});
 
-const services =[  {
-    name: 'transactions ',
-    img: './images/product-images/semo-vitta.png.png',
-    tag: 'send and recieve money'
-},
-]
+const services = [
+  {
+    name: "transactions ",
+    img: "./images/product-images/couple-laughing.png",
+    tag: "send and recieve money",
+  },
+
+  {
+    name: "bill payment",
+    img: "./images/product-images/decoder.png",
+    tag: "settle electricity and TV",
+  },
+  {
+    name: "settle invoice ",
+    img: "./images/product-images/medical-invoice.png",
+    tag: "settle payments with ease",
+  },
+
+  {
+    name: "pharmacy and drugs ",
+    img: "./images/product-images/drugs.png",
+    tag: "medical and health care",
+  },
+];
+
+const service = document.querySelector(".services-list");
+services.map((service_type) => {
+  service.innerHTML += `
+    <div class="service">
+    <div class="service-image">
+      <img src=${service_type.img} alt="">
+    </div>
+    <div class="service-detail">
+      <span class="service-detail-title">${service_type.name}</span>
+   <div class="service-tag">
+      <span> ${service_type.tag}</span>
+   </div>
+    </div>
+  </div>
+    `;
+});
